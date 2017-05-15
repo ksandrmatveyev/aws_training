865e711a2aed69de2682a0024f14722bc852f574
GOMEL\Aliaksandr_Matveyeu <Aliaksandr_Matveyeu@work.com>
2017-05-15 13:27:31 +0300

    deleted access of App1 to inernet, added comments

--- step2/gateway/iptables.sh
+++ step2/gateway/iptables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE &&\
-sudo iptables -A FORWARD -i eth2 -o eth0 -j ACCEPT &&\
-sudo iptables -A FORWARD -i eth0 -o eth2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eth1 -o eth0 -j REJECT &&\
+sudo iptables -A FORWARD -i eth0 -o eth1 -j REJECT &&\
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
